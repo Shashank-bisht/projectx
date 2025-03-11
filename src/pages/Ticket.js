3d48d38f0134df4db83a6cda7032c027311a5587
shashank-bisht <shashankbisht53734@gmail.com>
2025-03-12 04:44:08 +0530

    ui

--- src/pages/Ticket.js
+++ src/pages/Ticket.js
@@ -41,7 +41,7 @@ const Ticket = () => {
                 <div className='text-white underline mb-1 font-bold ml-52'>All tickets</div>
             </nav>
             {/* h1 */}
-            <div className="mt-52 bg-white mx-4 rounded-md h-[49%]">
+            <div className="mt-52 bg-white mx-4 rounded-md h-[48.5%]">
                 {showQRCode ? (
                     <div className="p-7" onClick={toggleQRCode}>
                         <QRCodeCanvas
@@ -62,7 +62,7 @@ const Ticket = () => {
                                 <p className='text-lg ml-5 mr-2 mt-2 inline-block'> {busDetails.busNumber}</p>
                             </div>
                             <div>
-                                <p className='text-lg ml-48 inline'>{'\u20B9'}{(busDetails.fare - (busDetails.fare / 10) + .25)}</p>
+                                <p className='text-lg ml-[190px] inline'>{'\u20B9'}{(busDetails.fare - (busDetails.fare / 10) + .25)}</p>
                             </div>
                         </div>
                         <hr className="border-t-1.5 mx-5 mt-2 border-black" />
@@ -81,7 +81,7 @@ const Ticket = () => {
                                 <p>{busDetails.routeNumber}</p>
                             </div>
                             <div className='ml-28 text-lg'>
-                                <p className='font-bold'>{'\u20B9'}{busDetails.fare}.0</p>
+                                <p className='font-semibold'>{'\u20B9'}{busDetails.fare}.0</p>
                             </div>
                         </div>
 
@@ -118,8 +118,8 @@ const Ticket = () => {
                         </div>
                         {/* qr code */}
                         <div>
-                            <div className='flex m-3 py-3 border-green-600 rounded-lg border-2 bg-green-100 items-center'>
-                                <QRCodeCanvas className='ml-24 mr-2'
+                            <div className='flex ml-3 mr-3 mb-3 mt-2 py-3 border-green-600 rounded-lg border-2 bg-green-100 items-center'>
+                                <QRCodeCanvas className='ml-28 mr-2'
                                     value="https://example.com" // Replace with your URL or text
                                     size={25}
                                     bgColor="#aee6ae" // Light green background
@@ -133,7 +133,7 @@ const Ticket = () => {
                     </>
                 )}
             </div>
-            <div className='mt-32 ml-36'>
+            <div className='mt-32 ml-36 pt-8'>
                 <div className='flex items-center'> <img className='w-11 h-[17px] inline' src='/im.png' /><p className='inline m-1 font-bold text-gray-600 '>NETWORK</p></div>
             </div>
         </div>
